aed51eca43077c155111951aeb67afc3b01edab3
SENAPBD <senapbd.com@gmail.com>
2025-02-13 23:13:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    alert("SENAP BD - আপনাকে স্বাগতম!");
-});
+// যদি প্রয়োজন হয়, অতিরিক্ত JS কোড এখানে যোগ করো
+
+console.log("SENAP BD Website Loaded!");
